b788997073c7443a45118c9a4485193f8d37e84f
xavierchen2021@outlook.com <xavierchen2021@outlook.com>
2026-09-08 20:57:53 +0800

    chore: 更新应用版本号至1.0.15

--- Sources/Model/AppVersion.swift
+++ Sources/Model/AppVersion.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// 应用版本号（打包脚本会自动替换此值）
 enum AppVersion {
-    static let current = "1.0.14"
+    static let current = "1.0.15"
     
     /// 从 Bundle 获取版本号，优先使用 Info.plist 中的值
     static var display: String {
